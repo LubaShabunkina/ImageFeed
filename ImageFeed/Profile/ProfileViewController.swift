42ffe8f06892c48a775c17d7212f68d18bf88fe3
LubaShabunkina <lubbroccoli@gmail.com>
2024-09-20 23:38:03 +0200

    added profile view controller

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Luba Shabunkina on 18/09/2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+}
---
+++ SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Luba Shabunkina on 20/09/2024.
+//
+
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
