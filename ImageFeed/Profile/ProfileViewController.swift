a4ab1e139597c1ca7cfb8a4c78fcade9204d6b12
LubaShabunkina <lubbroccoli@gmail.com>
2024-10-07 00:36:00 +0200

    Convert ProfileView layout from Storyboard to code

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -1,21 +1,122 @@
-//
-//  ProfileViewController.swift
-//  ImageFeed
-//
-//  Created by Luba Shabunkina on 18/09/2024.
-//
-
 import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var loginNameLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    private let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "Photo")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
     
-    @IBOutlet weak var logoutButton: UIButton!
+    private let loginNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "@ekaterina_nov"
+        label.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        label.textColor = UIColor(named: "YP Gray")
+        label.textAlignment = .left // Выравнивание текста
+        label.numberOfLines = 1 
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
     
-    @IBAction func didTapLogoutButton() {
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Екатерина Новикова"
+        label.textColor = UIColor(named: "YP White")
+        label.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        label.textAlignment = .left
+        
+        let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 23, weight: .bold),
+                .kern: -0.078 // Расстояние между буквами
+            ]
+            
+            label.attributedText = NSAttributedString(string: "Екатерина Новикова", attributes: attributes)
+            return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hello, world!"
+        label.font = UIFont.systemFont( ofSize: 13, weight: .regular)
+        label.textColor = UIColor(named: "YP White")
+    
+        return label
+    }()
+    
+    private let logoutButton: UIButton = {
+        let button = UIButton(type: .system)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setImage(UIImage(named: "Exit"), for: .normal)
+        button.tintColor = UIColor(named: "YP Red")
+            button.addTarget(self, action: #selector(didTapLogoutButton), for: .touchUpInside)
+            return button
+        }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "YP Black")
+        setupLayout()
+    }
+    
+    // Добавляем маску для углов в методе viewDidLayoutSubviews
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let path = UIBezierPath(roundedRect: avatarImageView.bounds,
+                                byRoundingCorners: [.topLeft],
+                                cornerRadii: CGSize(width: 61, height: 61))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        avatarImageView.layer.mask = mask
+    }
+    
+    private func setupLayout() {
+        view.addSubview(avatarImageView)
+        view.addSubview(loginNameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(descriptionLabel)
+        view.addSubview(logoutButton)
+        
+        NSLayoutConstraint.activate([
+            // Avatar ImageView
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+            avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 76),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            // Name Label
+            nameLabel.widthAnchor.constraint(equalToConstant: 235),
+            nameLabel.heightAnchor.constraint(equalToConstant: 18),
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 14),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            // Login Name Label
+            loginNameLabel.widthAnchor.constraint(equalToConstant: 99),
+            loginNameLabel.heightAnchor.constraint(equalToConstant: 18),
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            // Description Label
+            descriptionLabel.widthAnchor.constraint(equalToConstant: 77),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 18),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+
+            // Logout Button
+            logoutButton.widthAnchor.constraint(equalToConstant: 24),
+            logoutButton.heightAnchor.constraint(equalToConstant: 24),
+            logoutButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 99),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
+    }
+    @objc private func didTapLogoutButton() {
+        print("Logout tapped")
     }
 }
