23bf61f50efd56d68fa14bff75fa24a8024e9e1e
LubaShabunkina <lubbroccoli@gmail.com>
2024-10-21 01:04:49 +0200

    added progress view

---
+++ ImageFeed/New Group/OAuth2Service.swift
@@ -0,0 +1,14 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//  Created by Luba Shabunkina on 21/10/2024.
+//
+
+import Foundation
+
+final class OAuth2Service {
+    static let shared = OAuth2Service() // 1
+    private init() {}                   // 2
+
+}
